84d510469caeef64754a625fd96050391bb3f613
Superbil <superbil@gmail.com>
2012-08-05 01:11:39 +0800

    add brew checker

--- 06_grc.zsh
+++ 06_grc.zsh
@@ -1,3 +1,5 @@
-if [[ -x `brew --prefix grc` ]] then;
-   source "`brew --prefix grc`/etc/grc.bashrc"
+if [[ -x `which brew` ]]; then
+    if [[ -x `brew --prefix grc` ]]; then
+        source "`brew --prefix grc`/etc/grc.bashrc"
+    fi
 fi
